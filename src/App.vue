9ce4fb85572f76e14db32622918f9053d3a7c2ea
Shagun Sharma <119945335+Shagunsharmacs22@users.noreply.github.com>
2023-08-19 13:26:32 +0530

    main

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<!-- 
+<template>]
+<login />
+<Register />
+<TheHeader/>
+</template>
+<style></style>
+<script>
+import TheHeader from './components/layouts/TheHeader.vue'; 
+import login from '@/components/login.vue'; 
+import Register from './components/Register.vue';
+
+
+
+export default {
+  components: {
+    login,
+    Register,
+    TheHeader,
+  },
+  // ...
+};
+</script>
+ -->
---
+++ src/components/Shagun.vue
@@ -0,0 +1,7 @@
+<!-- 
+<template>
+<h1 class="text-3xl font-bold text-orange-800">
+hey this is Sharma dynasty
+</h1>
+
+</template> -->
